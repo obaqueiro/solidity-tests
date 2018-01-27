05b4c83429daabe51c8256d5c321c8b764bbee69
Omar Baqueiro Espinosa <obaqueiro@gmail.com>
2018-01-27 11:15:10 -0600

    Create sharedwallet.sol

---
+++ mortalcontract/sharedwallet.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.0;
+import "browser/mortalcontract.sol";
+
+contract SharedWallet is MortalContract {
+
+  mapping(address => Permission) addressMaping;
+  event addressAddedToSendersList(address whoAdded, address whoIsAllowed, uint allowedAmount);
+  
+  struct Permission {
+      bool isAllowed;
+      uint maxTransferAmount;
+  }
+  
+  function addAddressToSendersList(address permitted, uint maxTransferAmount) public onlyOwner {
+      addressMaping[permitted] = Permission(true, maxTransferAmount);
+      addressAddedToSendersList(msg.sender, permitted, maxTransferAmount);
+  }
+  
+  function sendFunds(address reciever, uint amountInWei) public {
+      if (addressMaping[msg.sender].isAllowed &&
+          addressMaping[msg.sender].maxTransferAmount >= amountInWei) {
+          reciever.transfer(amountInWei);
+      } 
+      else {
+          revert();
+      }
+  }
+  
+  function getCurrentAmount() public onlyOwner returns (uint) {
+    return this.balance;    
+  }
+  
+  function() public payable { 
+  }
+
+}
